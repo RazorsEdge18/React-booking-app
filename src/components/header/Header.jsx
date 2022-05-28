585c3b280c769892bb4fada6c7f57d7aa29adc2d
RazorsEdge18 <stefan.pesic10@gmail.com>
2022-05-28 18:11:01 +1000

    Added components for landing page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -40,7 +40,7 @@ function Header({type}) {
 
   return (
     <div className="header">
-      <div className="headerContainer">
+      <div className={type === "list" ? "headerContainer listMode" : "headerContainer"}>
         <div className="headerList">
           <div className="headerListItem active">
             <FontAwesomeIcon icon={faBed} />
